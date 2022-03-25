5dd86c8ab455d8fa4eca27af3cb5db8df52084c0
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-03-25 23:34:21 +0530

    extracted all data

--- scorecards.js
+++ scorecards.js
@@ -42,21 +42,26 @@ function getMatchDetails(html){
     console.log("Result of the match :- ",resultOfMatch);
 
     // 5) get innings
-    let allBatsmenTable = selecTool('.table.batsman tbody>tr');
+    let allBatsmenTable = selecTool('.table.batsman tbody');
     // console.log(allBatsmenRows.text());
     let htmlString = "";
     let count = 0;
     for(let i = 0;i < allBatsmenTable.length;i++){
         htmlString += selecTool(allBatsmenTable[i]).html();
-        console.log(count);
         let allRows = selecTool(allBatsmenTable[i]).find("tr");//Get the descendants(table rows) of each element(table) i.e this will get all <tr> in an ith table
         for(let j = 0;j < allRows.length;j++){
             let row = selecTool(allRows[j]);
             let firstColumnOfRow = row.find("td")[0];
             // hasClass() :- check to see if any of the matched elements have the given className
             if(selecTool(firstColumnOfRow).hasClass("batsman-cell")){
-                console.log(count);
-                count++;
+                let playerName = selecTool(row.find("td")[0]).text();
+                let runs = selecTool(row.find("td")[2]).text();
+                let balls = selecTool(row.find("td")[3]).text();
+                let numOf4 = selecTool(row.find("td")[5]).text();
+                let numOf6 = selecTool(row.find("td")[6]).text();
+                let sr = selecTool(row.find("td")[7]).text();
+
+               console.log(`${playerName} | ${runs} runs | ${balls} balls | ${numOf4} 4's | ${numOf6} 6's | ${sr} strike rate`);
             }
         }
     }
